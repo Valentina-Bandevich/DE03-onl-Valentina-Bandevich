1b1f875ce1a0f7e1a099157224ae5b6e33276f1b
DESKTOP-49T75RS\Dell <Valentina Bandevich>
2025-08-20 22:04:16 +0300

    HW8

---
+++ python/HW8/exercise.py
@@ -0,0 +1,38 @@
+
+#import numpy as pd
+""" Загрузите данные из CSV файла """
+
+import pandas as pd
+df=pd.read_csv(r'python/sales.csv')
+print(df)
+
+
+""" Выполните основные операции с данными: фильтрация,
+сортировка, агрегация с использованием pandas """
+
+prices = [10.99, 20.50, 15.75, 30.00, 25.00, 18.25, 22.10]
+df['price'] = prices
+print(df)
+
+count = [56, 24, 12, 5, 6, 23, 4]
+df['count'] = count
+print(df)
+
+df['total_price'] = df['price']* df['count']
+low_counterbale = df[df['count']<12]
+print(f'мало продаваемый товар: {low_counterbale}')
+
+sorted = df.sort_values(by='total_price')
+print(f'{sorted}')
+
+max_total_price = df.groupby('category')['total_price'].max()
+print(max_total_price)
+
+
+""" Применить numpy для числовых вычислений """
+
+avr_total_price = np.mean(df['total_price'])
+print(avr_total_price)
+
+value = np.median(df['total_price'])
+print(value)
