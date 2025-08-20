4702b75e17ce3110e745cfae4b7f0e065b8c96bc
Anastasiya Yafimuk <a.yafimuk@gmail.com>
2025-08-20 22:20:43 +0300

    HW6

---
+++ Python/HW6/HW6 module.py
@@ -0,0 +1,13 @@
+
+from HW6 import add, subtract, multiply, divide 
+a = 10
+b = 0
+result_1 = add(a, b)
+result_2 = subtract(a, b)
+result_3 = multiply(a, b)
+result_4 = divide(a, b)
+
+print(f'Сумма: {result_1}')
+print(f'Разность: {result_2}')
+print(f'Произведение: {result_3}')
+print(f'Частное: {result_4}')
---
+++ Python/HW6/sum.py
@@ -0,0 +1,12 @@
+from exercise import sum_with_params 
+from exercise import sum_list
+result = sum_with_params(7,3)
+print(result)
+
+my_list = [3,6,8]
+result2 = sum_list(my_list)
+print(result2)
+
+my_list2 = [5,8,2,2,2,55,90,13]
+result3 = sum_list(my_list2)
+print(result3)
